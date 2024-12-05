d5218a6cedf0e36de7c4ac800f023aae55eeac3b
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-12-05 13:44:43 +0100

    ADD HeaderComponent with basic structure and test
    Introduce a new HeaderComponent to the application, including its HTML template, TypeScript logic, and a basic test to ensure it creates successfully. The HeaderComponent is also integrated into the main app component, enhancing the modularity and organization of the application layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
