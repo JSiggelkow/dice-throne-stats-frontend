b74886d221c90893c21c7b59329b33c0cccda9d5
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-12-05 09:01:33 +0100

    DELETE HeaderComponent and update theme styles
    The header component has been removed along with its associated files and references in the app. This cleanup is part of the transition to a centralized theme management approach, where styles are now consolidated into a renamed theme file. The dark-theme.scss is updated and renamed to themes.scss to facilitate new theming capabilities.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatSlideToggleModule, HeaderComponent],
+  imports: [
+    MatToolbarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
